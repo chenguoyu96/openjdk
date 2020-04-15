a884d6336b1f33250b717924e881f0fb7bb2ac91
chenguoyu <chenguoyu96@foxmail.com>
2020-04-15 23:27:08 +0800

    提交代码

---
+++ jdk-test/src/main/java/org/chenguoyu/test/util/HashMapTest.java
@@ -0,0 +1,11 @@
+package org.chenguoyu.test.util;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap<String,Object> map = new HashMap<>();
+        map.put("key","value");
+        System.out.println(map);
+    }
+}
